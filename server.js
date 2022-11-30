d9555d765fe2ad69bd07c1a5f0e4be1e9d176fd2
Florian DAVID <florian.david@edu.univ-fcomte.fr>
2022-11-30 16:21:41 +0100

    Updating of server.js

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 const express = require('express')
 require('dotenv').config();
 const app = express();
+const departements = require('./services/departement.services').get_allDepartements();
+
+const  colRouter = require('./routes/col.router')
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(`${__dirname}/public`))
 
-app.set('views', `${__dirname}/views/`)
+app.set('views', `${__dirname}/views/`);
 app.set('view engine','pug');
 
+app.use('/cols', colRouter)
+
 app.get('/', (req, res) => {
-    res.render('home.pug');
+    res.render('home.pug', {departements});
 })
 
-const port = process.env.PORT || 2023;
-const host = process.env.HOST;
+const port = process.env.PORT || 8888;
 
-app.listen(port, host, () => {
-    console.log(`Listen to http://${host}:${port}`)
+app.listen(port, () => {
+    console.log(`Listen to http://localhost:${port}`)
 })
